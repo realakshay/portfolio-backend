945640280310b055d55bb721341073c6e93b5113
realakshay <gaikwadakshay663@gmail.com>
2025-12-26 17:42:34 +0530

    Added insert education route for admin

--- controller/admin.profile.controller.js
+++ controller/admin.profile.controller.js
@@ -1,5 +1,6 @@
 const Personal = require('../models/Personal');
 const Skill = require('../models/Skill');
+const Education = require('../models/Education');
 
 const insertPersonalInfo = async (req, res) => {
     const { fName, lName, mobile, email, linkedIn, github, shortBio, longBio } = req.body;
@@ -38,4 +39,30 @@ const insertSkillsInfo = async (req, res) => {
     }
 }
 
-module.exports = { insertPersonalInfo, insertSkillsInfo };
+const insertEducationInfo = async (req, res) => {
+    // Implementation for inserting education info
+    /** 
+     *  title: "Bachelors of Engineering",
+      university: "University of Pune",
+      date: " Jun 2015 - Jun 2019",
+      description: [
+        "Focused on Software Development, Data Structures, and Algorithms.",
+        "Completed with 8.41 CGPA",
+      ],
+    */
+    const { title, university, date, description } = req.body;
+    try {
+        const education = new Education({
+            title,
+            university,
+            date,
+            description
+        });
+        await education.save();
+        res.status(201).json({ message: "Education information inserted successfully" });
+    }catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+}
+
+module.exports = { insertPersonalInfo, insertSkillsInfo, insertEducationInfo };
--- routes/admin.profile.routes.js
+++ routes/admin.profile.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { insertPersonalInfo, insertSkillsInfo } = require('../controller/admin.profile.controller');
+const { insertPersonalInfo, insertSkillsInfo, insertEducationInfo } = require('../controller/admin.profile.controller');
 const router = express.Router();
 
 router.post('/insert/personal', insertPersonalInfo);
 router.post('/insert/skill', insertSkillsInfo);
+router.post('/insert/education', insertEducationInfo);
 
 module.exports = router;
