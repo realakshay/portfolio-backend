fb2051e1ee8e854add5ca8c711e31405adfb70b9
realakshay <gaikwadakshay663@gmail.com>
2025-12-29 18:06:26 +0530

    Get all contact route is now protected

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -4,8 +4,10 @@ const {
   getAllContacts,
 } = require("../controllers/contact.controller");
 
+const adminAuth = require('../middleware/adminAuth');
+
 const router = express.Router();
 router.post("/insert", addNewContact);
-router.get("/all", getAllContacts);
+router.get("/all", adminAuth, getAllContacts);
 
 module.exports = router;
